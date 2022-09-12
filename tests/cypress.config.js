2788467d45debc620dca8995f29ae6686694ba2c
wlaurentino <gama.william@gmail.com>
2022-09-12 08:55:25 -0300

    removendo allure reports e mongodb

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require('cypress');
 const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions')
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
-
-const mongo = require('cypress-mongodb')
-
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config)
-      mongo.configurePlugin(on)
       config = cypressBrowserPermissionsPlugin(on, config)
       return config
       // implement node event listeners here
@@ -21,10 +15,6 @@ module.exports = defineConfig({
       browserPermissions: {
         notifications: 'allow',
         geolocation: 'allow'
-      },
-      mongodb: {
-        'uri': 'mongodb+srv://qa:cademy@cluster0.rgul5f6.mongodb.net/QtruckDB?retryWrites=true&w=majority',
-        'database': 'QtruckDB'
       }
     }
   },
